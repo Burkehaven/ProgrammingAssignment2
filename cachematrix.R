4ef4f2e0dec67ac53e6b17cf13443eddbb40879a
Burkehaven <christine_e_hill@yahoo.com>
2019-08-12 20:17:52 -0400

    Comment makeCacheMatrix()
    Added comments to the file as a whole, to makeCacheMatrix() overall, and line by line inside makeCacheMatrix()

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions for caching a matrix and its inverse
+## - makeCacheMatrix interacts with the cached environment
+## -
 
-## Write a short comment describing this function
-
+## Input: a matrix, default, an empty matrix
+## Returns a list of functions for getting and setting
+## a matrix and its inverse in a cached environment
 makeCacheMatrix <- function(x = matrix()) {
-  # initialize the matrix inverse
+
+  ## initialize the matrix inverse
   my_inverse <- NULL
-  # create the set function
+
+  ## create a function for storing the original martrix
   set <- function(y) {
-  x <<- y
-  my_inverse <<- NULL
+    x <<- y
+    my_inverse <<- NULL
   }
+
+  ## get the original matrix
   get <- function() x
+
+  ## stores the matrix, which is calculated cacheSolve, below
   set_inverse <- function(new_inverse) my_inverse <<- new_inverse
+
+  ## get the inverse previously calculated
   get_inverse <- function() my_inverse
+
+  ## return the four functions above
   list(set = set, get = get, set_inverse = set_inverse,
        get_inverse = get_inverse)
 }
